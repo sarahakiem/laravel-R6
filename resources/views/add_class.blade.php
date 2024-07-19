68abac954e38c50e3972b7bffd4a112575f1da20
sarahakiem <saraamer964@gmail.com>
2024-07-20 02:52:56 +0300

    crud create&store task

--- resources/views/add_class.blade.php
+++ resources/views/add_class.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Class Information Form</title>
+    <title>Class new Form</title>
     <style>
         body {
             font-family: Arial, sans-serif;
